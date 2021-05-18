4d0d1f2a2127a44db9b3aac9170e8e2371929b84
brendanatme <registrations@brendanat.me>
2021-05-18 19:41:18 -0400

    fix: fetch json async bug

--- src/wrap-fetch.ts
+++ src/wrap-fetch.ts
@@ -28,6 +28,16 @@ export const wrapFetch = (
     ...defaultOptions,
   };
 
+  /**
+   * the base function we'll use to make all fetch calls. it:
+   * - merges the configs (1st priority: function arg; 2nd priority: api config)
+   * - runs interceptors to modify config at request time
+   * - calls 'beforeHook'
+   * - runs fetch
+   * - calls 'afterHook'
+   * - cleans up data
+   * - runs transformers on data and return data
+   */
   const _fetch = async (method: Methods, url: string, options: any = {}) => {
     const requestUrl = `${baseUrl}${url}`;
     const requestConfig = interceptors.reduce((result, next) => next(result), {
@@ -53,13 +63,23 @@ export const wrapFetch = (
       }));
     }
 
-    const data = response.status !== 204 ? response.json() : response;
+    const data = response.status !== 204 ? await response.json() : response;
 
     return transformers.reduce((result, next) => next(result), data);
   };
 
+  /**
+   * a higher-order function we can use to create publicly-exposed api methods
+   * if its a simple request with no payload arguments, like a GET or DELETE,
+   * use this
+   */
   const makeGetFn = (method: Methods) => <P>(url: string, options: any = {}): Promise<P> => _fetch(method, url, options);
 
+  /**
+   * a higher-order fucntion we can use to create publicly-exposed api methods
+   * for requests that need payload arguments, like POST, PUT, and PATCH,
+   * use this
+   */
   const makePostFn = (method: Methods) => async <P>(url: string, data: any, options: any = {}): Promise<P> => {
     const { headers, ...remainder } = options
 
@@ -69,12 +89,29 @@ export const wrapFetch = (
         ...POST_HEADERS,
         ...headers,
       },
-      ...remainder
+      ...remainder,
     });
 
     return results;
   };
 
+  /**
+   * expose the public methods of our returned api object
+   * 
+   * 5 api methods:
+   * 
+   * - del(url)
+   * - get(url)
+   * - patch(url, body)
+   * - post(url, body)
+   * - put(url, body)
+   * 
+   * and 3 configuration methods:
+   * 
+   * - configure(config) update the config
+   * - intercept(fn) add a function to modify the config at request-time
+   * - transform(fn) add a function to modify the data output at request-time
+   */
   return {
     configure(options: any): void {
       config = {
